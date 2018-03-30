490e742852b3cc0a2bccd480db5ae7edc136e5db
6407001499 <yi.tongtong@zte.com.cn>
2018-03-30 15:32:26 +0800

    Initial Commit

---
+++ Algorithm/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Algorithm
+//
+//  Created by zte's iMac on 2018/3/30.
+//  Copyright © 2018年 zte's iMac. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
